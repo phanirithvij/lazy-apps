6d67d550b6d999b10fa8e4fc24b76a53bb9fda7f
Robert Helgesson <robert@rycee.net>
2023-10-31 08:09:38 +0100

    Apply nixfmt

--- flake.nix
+++ flake.nix
@@ -1,9 +1,7 @@
 {
   description = "Lazy Apps";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  };
+  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable"; };
 
   outputs = { self, nixpkgs }:
     let forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
@@ -14,56 +12,57 @@
 
           lib = pkgs.lib;
 
-          mkExeName =  pkg:
-            if pkg == null
-              then null else pkg.meta.mainProgram or (lib.getName pkg);
+          mkExeName = pkg:
+            if pkg == null then
+              null
+            else
+              pkg.meta.mainProgram or (lib.getName pkg);
 
         in {
-          lazy-app = pkgs.makeOverridable ({ pkg ? null, exe ? mkExeName pkg, desktopItem ? null }:
-            pkgs.runCommand "lazy-${exe}-${lib.getVersion pkg}" (let
-              exePath =
-                if exe != null then
+          lazy-app = pkgs.makeOverridable
+            ({ pkg ? null, exe ? mkExeName pkg, desktopItem ? null }:
+              pkgs.runCommand "lazy-${exe}-${lib.getVersion pkg}" (let
+                exePath = if exe != null then
                   "${lib.getBin pkg}/bin/${exe}"
                 else
                   lib.getExe pkg;
 
-              notify-send = lib.getExe pkgs.libnotify;
-            in {
-              nativeBuildInputs = [ pkgs.copyDesktopItems ];
+                notify-send = lib.getExe pkgs.libnotify;
+              in {
+                nativeBuildInputs = [ pkgs.copyDesktopItems ];
 
-              desktopItems = lib.optional (desktopItem != null) desktopItem;
+                desktopItems = lib.optional (desktopItem != null) desktopItem;
 
-              script = ''
-                #!${pkgs.runtimeShell}
+                script = ''
+                  #!${pkgs.runtimeShell}
 
-                set -euo pipefail
+                  set -euo pipefail
 
-                app='${exe}'
-                path='${builtins.unsafeDiscardStringContext exePath}'
+                  app='${exe}'
+                  path='${builtins.unsafeDiscardStringContext exePath}'
 
-                if [[ ! -e $path ]]; then
-                    noteId=$(${notify-send} -t 0 -p "Realizing $app …")
-                    trap "${notify-send} -r '$noteId' 'Canceled realization of $app'" EXIT
-                    SECONDS=0
-                    nix-store --realise "$path" > /dev/null 2>&1
-                    trap - EXIT
-                    ${notify-send} -r "$noteId" "Realized $app in $SECONDS s"
-                fi
+                  if [[ ! -e $path ]]; then
+                      noteId=$(${notify-send} -t 0 -p "Realizing $app …")
+                      trap "${notify-send} -r '$noteId' 'Canceled realization of $app'" EXIT
+                      SECONDS=0
+                      nix-store --realise "$path" > /dev/null 2>&1
+                      trap - EXIT
+                      ${notify-send} -r "$noteId" "Realized $app in $SECONDS s"
+                  fi
 
-                if [[ -e $path ]]; then
-                    exec $path "$@"
-                fi
-              '';
-              exeName = exe;
-              passAsFile = [ "script" ];
-            }) ''
-              runHook preInstall
-              install -Dm755 "$scriptPath" "$out/bin/$exeName"
-              runHook postInstall
-            '') { };
+                  if [[ -e $path ]]; then
+                      exec $path "$@"
+                  fi
+                '';
+                exeName = exe;
+                passAsFile = [ "script" ];
+              }) ''
+                runHook preInstall
+                install -Dm755 "$scriptPath" "$out/bin/$exeName"
+                runHook postInstall
+              '') { };
 
-          examples = let
-            lazy-app = self.packages.${system}.lazy-app;
+          examples = let lazy-app = self.packages.${system}.lazy-app;
           in pkgs.symlinkJoin {
             name = "lazy-apps-examples";
             paths = [
