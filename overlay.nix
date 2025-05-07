53432d762136b0f4d8e936de3134abe128978027
Robert Helgesson <robert@rycee.net>
2025-05-07 10:10:29 +0200

    Only include lazy apps in overlay

--- overlay.nix
+++ overlay.nix
@@ -1,16 +1,27 @@
-final: prev: {
+final: prev:
+let
+  lazy-app = ((import ./.).mkLazyApps { pkgs = final; }).lazy-app;
+in
+{
+  lazy-app = lazy-app;
+
   lazyApps =
     let
       lib = prev.lib;
 
-      lazy-app = ((import ./.).mkLazyApps { pkgs = final; }).lazy-app;
-
-      # Recursively replace each derivation having `mainProgram` set with a lazy application.
+      # Recursively replace each derivation having `mainProgram` set with a lazy
+      # application. Any other attribute will be given a null value, which can
+      # be filtered out.
       mkLazyPkg =
         name: value:
         if lib.isAttrs value then
-          if lib.isDerivation value && (value.meta or { }) ? mainProgram then
-            lazy-app.override { pkg = value; }
+          if lib.isDerivation value then
+            # If the derivation has a main program, then create a corresponding
+            # lazy application.
+            if (value.meta or { }) ? mainProgram then
+              lazy-app.override { pkg = value; }
+            else
+              throw "This is not a valid lazy derivation: ${name}"
           else
             mkLazyPkgs value
         else
