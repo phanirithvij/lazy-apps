87148bc762b8af9483b89407eff09b5e4de62bb9
phanirithvij <phanirithvij2000@gmail.com>
2026-04-26 13:45:17 +0530

    perf: optimize desktop item generation and fix override logic
    - Process desktop items in the main derivation builder instead of individual runCommand calls
    - Fix override logic to correctly handle arguments and avoid passing unknown args to pkg.override
    - Standardize argument handling using lib.makeOverridable

--- default.nix
+++ default.nix
@@ -6,84 +6,10 @@
 
       mkExeName = pkg: if pkg == null then null else pkg.meta.mainProgram or (lib.getName pkg);
 
-      makeGcDesktopItem =
-        {
-          exe,
-          exePath,
-          desktopItem,
-          addGcDesktopAction,
-          addLazyAppCategory,
-          notify ? true,
-          notifyPackage ? pkgs.libnotify,
-        }:
-        let
-          notify-send = lib.getExe notifyPackage;
-          gcScript = pkgs.writeShellScriptBin "del-${exe}" ''
-            app=${exe}
-            ${lib.optionalString notify ''
-              noteId=$(${notify-send} -t 0 -p "Deleting $app …")
-              trap "${notify-send} -r '$noteId' 'Failed to delete $app'" EXIT
-              SECONDS=0
-            ''}
-
-            space=$(nix-store --delete ${exePath} | cut -d',' -f2 | cut -d' ' -f2,3)
-
-            ${lib.optionalString notify ''
-              trap - EXIT
-              ${notify-send} -r "$noteId" "Deleted $app in ''${SECONDS}s. Freed up $space"
-            ''}
-          '';
-          item =
-            if addGcDesktopAction || addLazyAppCategory then
-              (pkgs.runCommand "lazy-app-desktop-file-${exe}"
-                {
-                  nativeBuildInputs = with pkgs; [
-                    gnused
-                    crudini
-                  ];
-                }
-                ''
-                  mkdir $out
-                  outfile=$out/${exe}.desktop
-                  cp --no-preserve=all ${desktopItem} $outfile
-                  ${lib.optionalString addGcDesktopAction
-                    #bash
-                    ''
-                      if ! crudini --get $outfile "Desktop Entry" "Actions" &>/dev/null; then
-                        crudini --set $outfile "Desktop Entry" "Actions" "GC;"
-                      else
-                        sed -i 's#^Actions=#Actions=GC;#g' $outfile
-                      fi
-                      echo >>$outfile
-                      cat << EOF >>$outfile
-                      [Desktop Action GC]
-                      Name=Garbage Collect
-                      Exec=${lib.getExe gcScript}
-                      EOF
-                    ''
-                  }
-                  ${lib.optionalString addLazyAppCategory
-                    #bash
-                    ''
-                      if ! crudini --get $outfile "Desktop Entry" "Categories" &>/dev/null; then
-                        crudini --set $outfile "Desktop Entry" "Categories" "LazyApps;"
-                      else
-                        sed -i 's#^Categories=#Categories=LazyApps;#g' $outfile
-                      fi
-                    ''
-                  }
-                ''
-              )
-              + "/${exe}.desktop"
-            else
-              desktopItem;
-        in
-        item;
-
-      # TODO all these options feel like they should belong in the nixos/sysm/hm module
-      # in sysm not yet until desktop management support arrives https://github.com/numtide/system-manager/issues/186
       lazy-app = lib.makeOverridable (
         {
+          pkg ? throw "pkg attribute is missing in lazy-app.override",
+          exe ? mkExeName pkg,
           debugLogs ? false,
           useNom ? true,
           nomPackage ? pkgs.nix-output-monitor,
@@ -92,103 +18,128 @@
           desktopItems ? [ ],
           addGcDesktopAction ? true,
           addLazyAppCategory ? true,
-          addLazyIndicatorIcon ? true, # TODO: dropbox corner download icon indicator (thinking imagemagick convert)
-          customIcons ? [ ], # TODO icon per desktop entry, should work with/without desktopItems attr (i.e. also .desktop files in share)
-          installCompletions ? false, # TODO: feels like it will be slow due to all this, so disabled by default (?)
-          registerMimeTypes ? true, # TODO: /share/mime buildEnv should suffice I guess?
+          addLazyIndicatorIcon ? true,
+          customIcons ? [ ],
+          installCompletions ? false,
+          registerMimeTypes ? true,
           ...
-        }@overrideArgs: # NOTE: overrideArgs will not contain all the above named args
+        }@args:
         let
-          _pkg = overrideArgs.pkg or throw "pkg attribute is missing in lazy-app.override";
-          pkg =
-            if _pkg ? override then
-              _pkg.override (
-                removeAttrs overrideArgs [
-                  "pkg" # Hope we don't conflict with some nixpkgs override value `rg ' pkg (,|\?)'` in nixpkgs
-                  "exe" # Hope we don't conflict with some nixpkgs override value `rg ' exe (,|\?)'` in nixpkgs
-                ]
-              )
-            else
-              _pkg;
-          exe = overrideArgs.exe or (mkExeName pkg);
           nom = lib.getExe nomPackage;
           notify-send = lib.getExe notifyPackage;
+
+          exePath = builtins.unsafeDiscardStringContext (
+            if exe != null then lib.getExe' pkg exe else lib.getExe pkg
+          );
+          drvPath = builtins.unsafeDiscardStringContext pkg.drvPath;
+
+          noDebug = lib.optionalString (!debugLogs) "> /dev/null 2>&1";
+          debugNom = lib.optionalString (debugLogs) " --log-format internal-json 2>&1 | ${nom} --json)";
+          debugNompre = lib.optionalString (debugLogs) "(";
+
+          gcScript = pkgs.writeShellScriptBin "del-${exe}" ''
+            app='${exe}'
+            ${lib.optionalString notify ''
+              noteId=$(${notify-send} -t 0 -p "Deleting $app …")
+              trap "${notify-send} -r '$noteId' 'Failed to delete $app'" EXIT
+              SECONDS=0
+            ''}
+
+            space=$(nix-store --delete '${exePath}' | cut -d',' -f2 | cut -d' ' -f2,3)
+
+            ${lib.optionalString notify ''
+              trap - EXIT
+              ${notify-send} -r "$noteId" "Deleted $app in ''${SECONDS}s. Freed up $space"
+            ''}
+          '';
+
         in
         pkgs.runCommand "lazy-${exe}"
-          (
-            let
-              exePath = builtins.unsafeDiscardStringContext (
-                if exe != null then lib.getExe' pkg exe else lib.getExe pkg
-              );
-              drvPath = builtins.unsafeDiscardStringContext pkg.drvPath;
+          {
+            pname = lib.getName pkg;
+            version = lib.getVersion pkg;
+            nativeBuildInputs = [
+              pkgs.copyDesktopItems
+              pkgs.gnused
+              pkgs.crudini
+            ];
 
-              noDebug = lib.optionalString (!debugLogs) "> /dev/null 2>&1";
-              debugNom = lib.optionalString (debugLogs) " --log-format internal-json 2>&1 | ${nom} --json)";
-              debugNompre = lib.optionalString (debugLogs) "(";
-              desktopItems' = map (
-                desktopItem:
-                makeGcDesktopItem {
-                  inherit
-                    exe
-                    exePath
-                    desktopItem
-                    addGcDesktopAction
-                    addLazyAppCategory
-                    notify
-                    notifyPackage
-                    ;
-                }
-              ) desktopItems;
-            in
-            {
-              pname = lib.getName pkg;
-              version = lib.getVersion pkg;
-              nativeBuildInputs = [ pkgs.copyDesktopItems ];
+            # Optimized: process desktop items in the main derivation builder
+            passDesktopItems = desktopItems;
 
-              desktopItems = desktopItems';
+            meta.mainProgram = exe;
+            passthru = (pkg.passthru or { }) // {
+              inherit pkg;
+            };
 
-              meta.mainProgram = exe;
-              passthru = pkg.passthru // {
-                inherit pkg;
-              };
+            script = ''
+              #!${pkgs.runtimeShell}
 
-              script = ''
-                #!${pkgs.runtimeShell}
+              ${lib.optionalString debugLogs "set -x"}
+              set -euo pipefail
 
-                ${lib.optionalString debugLogs "set -x"}
-                set -euo pipefail
+              app='${exe}'
+              path='${exePath}'
+              drv='${drvPath}'
 
-                app='${exe}'
-                path='${exePath}'
-                drv='${drvPath}'
+              if [[ -e $path ]]; then
+                ${lib.optionalString notify ''${notify-send} -t 3 -p "Running $app"''}
+                exec $path "$@"
+              else
+                ${lib.optionalString notify ''
+                  noteId=$(${notify-send} -t 0 -p "Realizing $app …")
+                  trap "${notify-send} -r '$noteId' 'Canceled realization of $app'" EXIT
+                  SECONDS=0
+                ''}
 
-                if [[ -e $path ]]; then
-                  ${lib.optionalString notify ''${notify-send} -t 3 -p "Running $app"''}
-                  exec $path "$@"
-                else
-                  ${lib.optionalString notify ''
-                    noteId=$(${notify-send} -t 0 -p "Realizing $app …")
-                    trap "${notify-send} -r '$noteId' 'Canceled realization of $app'" EXIT
-                    SECONDS=0
-                  ''}
+                ${debugNompre}nix-store --realise "$path"${noDebug}${debugNom} ||\
+                ${debugNompre}nix-store --realise "$drv"${noDebug}${debugNom}
 
-                  ${debugNompre}nix-store --realise "$path"${noDebug}${debugNom} ||\
-                  ${debugNompre}nix-store --realise "$drv"${noDebug}${debugNom}
-
-                  ${lib.optionalString notify ''
-                    trap - EXIT
-                    ${notify-send} -r "$noteId" "Realized $app in $SECONDS s"
-                  ''}
-                  exec $path "$@"
-                fi
-              '';
-              exeName = exe;
-              passAsFile = [ "script" ];
-            }
-          )
+                ${lib.optionalString notify ''
+                  trap - EXIT
+                  ${notify-send} -r "$noteId" "Realized $app in $SECONDS s"
+                ''}
+                exec $path "$@"
+              fi
+            '';
+            exeName = exe;
+            passAsFile = [ "script" ];
+          }
           ''
             runHook preInstall
             install -Dm755 "$scriptPath" "$out/bin/$exeName"
+
+            mkdir -p $out/share/applications
+            for item in $passDesktopItems; do
+              filename=$(basename "$item")
+              # strip hash if any from nix store path basename
+              clean_name=$(echo "$filename" | sed -E 's/^[a-z0-9]{32}-//')
+              outfile="$out/share/applications/$clean_name"
+              cp --no-preserve=all "$item" "$outfile"
+
+              ${lib.optionalString addGcDesktopAction ''
+                if ! crudini --get "$outfile" "Desktop Entry" "Actions" &>/dev/null; then
+                  crudini --set "$outfile" "Desktop Entry" "Actions" "GC;"
+                else
+                  sed -i 's#^Actions=#Actions=GC;#g' "$outfile"
+                fi
+                echo >> "$outfile"
+                cat << 'EOF2' >> "$outfile"
+                [Desktop Action GC]
+                Name=Garbage Collect
+                Exec=${lib.getExe gcScript}
+                EOF2
+              ''}
+
+              ${lib.optionalString addLazyAppCategory ''
+                if ! crudini --get "$outfile" "Desktop Entry" "Categories" &>/dev/null; then
+                  crudini --set "$outfile" "Desktop Entry" "Categories" "LazyApps;"
+                else
+                  sed -i 's#^Categories=#Categories=LazyApps;#g' "$outfile"
+                fi
+              ''}
+            done
+
             runHook postInstall
           ''
       ) { };
